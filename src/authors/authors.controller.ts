7b23891f98ca286ce6459dc45f08e138e310f446
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2023-01-05 19:50:31 +0100

    Add authors module

---
+++ src/authors/authors.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
+@Controller('authors')
+export class AuthorsController {
+  constructor(private authorsService: AuthorsService) {}
+}
